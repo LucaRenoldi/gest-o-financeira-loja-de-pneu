2b37d7eed1ee850f6abb3d3f57494e630fe59f69
luca-renoldi <lrenoldi@hcvtechnologies.com>
2026-04-17 15:20:21 -0300

    fix: ajustando o build

--- pneu-control/vite.config.js
+++ pneu-control/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000, // Aumenta o limite para 1MB antes de avisar
+  },
 })
